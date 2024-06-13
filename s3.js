050bbd44c4c57c9bf5321cfc5766234105d2ce44
husain_batatawala <husainbatatawala@gofynd.com>
2024-06-14 01:57:23 +0530

    upgraded s3 library to 3 and added ceph support

--- s3.js
+++ s3.js
@@ -1,65 +1,77 @@
-const { S3 } = require('aws-sdk');
+const { S3Client, GetObjectCommand, PutObjectCommand, CreatePresignedPostCommand, HeadObjectCommand } = require('@aws-sdk/client-s3');
 
 module.exports = class S3Storage {
 
     constructor(config){
-        this.s3 = new S3(config);
+        const config = {
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            },
+            forcePathStyle: true,
+        };
+        if (process.env.ENDPOINT) {
+            config.endpoint = process.env.ENDPOINT;
+            config.region = "us-east-1";
+        } else {
+            if (!process.env.AWS_REGION) 
+                throw new Error("AWS_REGION Environment variable not found.");
+            config.region = process.env.AWS_REGION;
+        }
+        this.s3 = new S3Client(config);
     }
 
     uploadFile = (params) => {
         const { buffer, bucket, acl, filePath } = params;
-        const s3UploadRequest = this.s3.upload({
+        const uploadParams = {
             Bucket: bucket,
-            Key: `${filePath}`,
-            Body : buffer,
+            Key: filePath,
+            Body: buffer,
             ACL: acl,
-        });
-        
-        return new Promise((resolve, reject) => {
-            s3UploadRequest
-            .promise()
-            .then(result => resolve({
-                Location : result.Location,
-                Bucket : result.Bucket,
-                Key : result.Key
-            }))
-            .catch(err => reject(err));
-        });
+        };
 
+        return new Promise((resolve, reject) => {
+            this.s3.send(new PutObjectCommand(uploadParams))
+                .then(result => resolve({
+                    Location: `https://${bucket}.s3.amazonaws.com/${filePath}`,
+                    Bucket: result.Bucket,
+                    Key: result.Key
+                }))
+                .catch(err => reject(err));
+        });
     }
 
-    getSignedUrl = (filePath, options) => {
-        const { 
-            operation = 'getObject', 
-            acl = 'private',
-            contentType, 
-            bucket, 
-            expires,
-        } = options;
-
+    getSignedUrl = async (filePath, options) => {
+        const { operation = 'getObject', acl = 'private', contentType, bucket, expires } = options;
         const params = {
             Bucket: bucket,
-            Expires: expires,
-            Key: filePath
+            Key: filePath,
+            Expires: expires
         };
+
         if (operation === 'putObject') {
             params.ACL = acl;
             params.ContentType = contentType || 'binary/octet-stream';
         }
 
-        return new Promise((resolve, reject) => {
-            if(operation === 'putObject') {
-                resolve(this.s3.getSignedUrl(operation, params))
+        try {
+            if (operation === 'putObject') {
+                const command = new CreatePresignedPostCommand(params);
+                const signedUrl = await this.s3.getSignedUrl(command);
+                return signedUrl;
+            } else {
+                await this.s3.send(new HeadObjectCommand({ Bucket: bucket, Key: filePath }));
+                const command = new GetObjectCommand(params);
+                const signedUrl = await this.s3.getSignedUrl(command);
+                return signedUrl;
             }
-            this.s3.headObject({ Bucket : bucket, Key : filePath }).promise()
-            .then(_ => resolve(this.s3.getSignedUrl(operation, params)))
-            .catch(error => reject(error))
-        })
+        } catch (error) {
+            throw error;
+        }
     }
 
-    generateSignedPostPolicy = (params) => {
+    generateSignedPostPolicy = async (params) => {
         const { bucket, expires, key, fields = {}, conditions = [] } = params;
-
         const options = {
             Bucket: bucket,
             Conditions: [
@@ -72,27 +84,29 @@ module.exports = class S3Storage {
             Expires: expires,
         };
 
-        return new Promise((resolve, reject) => {
-            this.s3.createPresignedPost(options, (err, data) => {
-                if(err) reject(err);
-                data.fields.key = key;
-                resolve(data);
-            });
-        })
+        try {
+            const command = new CreatePresignedPostCommand(options);
+            const data = await this.s3.send(command);
+            data.fields.key = key;
+            return data;
+        } catch (error) {
+            throw error;
+        }
     }
 
     getFileBuffer = async (filePath, params) => {
         const { bucket } = params;
+        const getObjectParams = {
+            Bucket: bucket,
+            Key: filePath
+        };
 
-        return new Promise((resolve, reject) => {
-            this.s3.getObject({
-                Bucket : bucket,
-                Key : filePath
-            })
-            .promise()
-            .then(res => resolve(res.Body))
-            .catch(error => reject(error));
-        })
+        try {
+            const command = new GetObjectCommand(getObjectParams);
+            const data = await this.s3.send(command);
+            return data.Body;
+        } catch (error) {
+            throw error;
+        }
     }
-    
 }
